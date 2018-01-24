7f81e399a0ebde2d05308654cdb33ddce6b35824
leonardonogueira94 <leonardo.nogueira94@live.com>
2018-01-24 21:29:16 -0200

    Opções inciais sidemenu

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Cloudelivery - Painel')
+
+@section('content_header')
+    <h1>Painel</h1>
+@stop
+
+@section('content')
+    <p>You are logged in!</p>
+@stop
